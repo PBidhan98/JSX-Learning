fc57624b006b12bb0eaf4e99c437bf3ddc67f935
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-13 12:31:15 +0530

    JSX Learning

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+//For getting content to show on the screen
+//Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create a react Component
+const App = () => {
+  const buttonText = 'Click Me';
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter Name:</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor:'blue', color:'white'}}>{buttonText}</button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Take the react Component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
